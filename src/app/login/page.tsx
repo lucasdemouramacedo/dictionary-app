f40cd5c9048864e51875c69e16f462c7433e0bb5
lucasdemouramacedo <lucasdemouramacedo@gmail>
2025-07-26 14:09:49 -0300

    first commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+export const metadata = {
+    title: 'Login',
+    description: 'Login to your account',
+};
+
+export default function Login() {
+    return (
+        <div>Login</div>
+    )
+}
---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,9 @@
+export default function Input() {
+    return (
+        <input
+            type="text"
+            className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Type here..."
+        />
+    );
+}
